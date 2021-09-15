bb0ef55e2fd0d57be5aeee6c9cd76d58c5954f5b
ug-code <ugur.gunes@logo.com.tr>
2021-09-15 14:24:27 +0300

    check api v1

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('/v1', function () {
+    return [
+    	'data'='Welcome to the Desert of the Real'];
+});
