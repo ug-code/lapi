d94ab07f64f58f02f305d08f64a5ff0ab70c71a4
ug-code <ugurgunes@windowslive.com>
2021-09-15 18:39:27 +0300

    update route api

--- routes/api.php
+++ routes/api.php
@@ -14,12 +14,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::middleware('auth:sanctum')
+     ->get('/user', function(Request $request) {
+         return $request->user();
+     });
 
 
-Route::get('/v1', function () {
+Route::get('/v1', function() {
     return [
-    	'data'=>'Welcome to the Desert of the Real'];
+        'data' => 'Welcome to the Desert of the Real'
+    ];
 });
