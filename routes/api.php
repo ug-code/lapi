0df010a63849c70adb657430aa288abec4dc75f5
ug-code <ugur.gunes@logo.com.tr>
2021-09-15 14:25:18 +0300

    Fixed api v1 test

--- routes/api.php
+++ routes/api.php
@@ -21,5 +21,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::get('/v1', function () {
     return [
-    	'data'='Welcome to the Desert of the Real'];
+    	'data'=>'Welcome to the Desert of the Real'];
 });
