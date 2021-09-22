1ac3b5969104ead1f476b3d4ef31195271137d7a
ug-code <ugurgunes@windowslive.com>
2021-09-22 17:10:53 +0300

    Update
    update Prodfile
    add telescope

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,6 +24,9 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
+        $schedule->call(function () {
+            echo "Hi";
+        })->everyMinute();
         // $schedule->command('inspire')->hourly();
     }
 
