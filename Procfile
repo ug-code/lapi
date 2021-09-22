3b3d3c204d4bc41dbe88faae625578be659ae35e
ug-code <ugurgunes@windowslive.com>
2021-09-22 23:46:11 +0300

    schedular
    run schedular for Procfile

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 web: vendor/bin/heroku-php-apache2 public/.
-scheduler: php -d memory_limit=512M artisan schedule:run
+scheduler: php -d memory_limit=512M artisan schedule:cron
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        Commands\RunScheduler::class
     ];
 
     /**
@@ -24,9 +24,6 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->call(function () {
-            echo "Hi";
-        })->everyMinute();
         // $schedule->command('inspire')->hourly();
     }
 
