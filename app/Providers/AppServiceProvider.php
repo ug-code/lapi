5201ffa4f8775debf9be27c578acb11056353c32
ug-code <ugurgunes@windowslive.com>
2021-09-23 21:53:59 +0300

    fixed bugs
    syntax error for AppService

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,7 +26,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         if (!App::environment('local')) {
-            URL::forceSchema('https');
+            URL::forceScheme('https');
         }
     }
 }
