99304f08b0429b248e5c329024e0b93862e18e3e
ug <ugurgunes@windowslive.com>
2024-11-06 02:52:21 +0300

    Update console.php

--- routes/console.php
+++ routes/console.php
@@ -6,3 +6,11 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+
+
+Schedule::call(function () {
+    echo "Hi";
+})->name('say-hi')->daily();
+
+
