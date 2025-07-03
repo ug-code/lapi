f0157eb5e90d7a8b3f2bd82a8fe3ba60af07d4cd
ug <ugurgunes@windowslive.com>
2025-07-03 16:39:32 +0300

    Fix namespace and import for CalculateRequest
    Updated the namespace of CalculateRequest to App\Http\Requests\Inflation and corrected its import in InflationController to match the new location.

--- app/Http/Controllers/InflationController.php
+++ app/Http/Controllers/InflationController.php
@@ -2,8 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\CalculateRequest;
-use Illuminate\Http\Request;
+use App\Http\Requests\Inflation\CalculateRequest;
 use App\Models\TuikMonthlyData;
 
 class InflationController extends Controller
