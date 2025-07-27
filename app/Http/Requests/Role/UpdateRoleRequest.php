72c083e91977002678bd55573ca4f30d35915bbf
ug <ugurgunes@windowslive.com>
2025-07-27 17:38:41 +0300

    Update UpdateRoleRequest.php

--- app/Http/Requests/Role/UpdateRoleRequest.php
+++ app/Http/Requests/Role/UpdateRoleRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests\Role;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class UpdateRoleRequest extends FormRequest
 {
@@ -11,8 +12,13 @@ class UpdateRoleRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => 'required|string|max:255|unique:roles,name,' . $this->role,
-            'guard_name' => 'sometimes|string|max:255'
+            'name' => [
+                'required',
+                'string',
+                'max:255',
+                Rule::unique('roles', 'name')->ignore($this->route('id')),
+            ],
+            'guard_name' => 'sometimes|string|max:255',
         ];
     }
-} 
+}
